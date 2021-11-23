d532c6b367cf71adf842858c6ffa38a5e9a60852
thanhlongtld <thanhlongtld@gmail.com>
2021-11-23 18:00:54 +0700

    first push

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+
+const Home: NextPage = () => {
+    return <p className='text-2xl text-red-200'>Hello</p>;
+};
+
+export default Home;
