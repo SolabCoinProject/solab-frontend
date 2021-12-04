1f574eef8a65cec7c736d57ad5a69b6dc975eaf8
thanhlongtld <thanhlongtld@gmail.com>
2021-12-04 17:27:40 +0700

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 
 const Home: NextPage = () => {
-    return <p className='text-2xl text-red-200'>Test</p>;
+    return <p className='text-2xl text-blue-500'>Test</p>;
 };
 
 export default Home;
