864d962420aaf4b2d8b1fbdd56e55a079239cb89
thanhlongtld <thanhlongtld@gmail.com>
2022-01-16 09:57:29 +0700

    update

--- src/config/stake.ts
+++ src/config/stake.ts
@@ -1,3 +1,5 @@
 export const stakePubKey = '8YCYsM6oaFeHDr7nxHMFn6w3xURDza7MWP2Zg4Q3mEiT';
-export const stakeInterest = 1;
-export const receiveFeePubKey = process.env.NEXT_PUBLIC_RECEIVE_FEE_PUBKEY || '8dmwMpEt9nxicuy5AcxZmnyGV9xALTZVBhGejreKQLBf';
+export const stakeInterest = 1.3;
+export const receiveFeePubKey =
+    process.env.NEXT_PUBLIC_RECEIVE_FEE_PUBKEY ||
+    '8dmwMpEt9nxicuy5AcxZmnyGV9xALTZVBhGejreKQLBf';
